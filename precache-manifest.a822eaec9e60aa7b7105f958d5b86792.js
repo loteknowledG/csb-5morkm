315cd69245ec79ced4ee1e957e1c37e59dd1f26c
Code Sandbox <hello@codesandbox.io>
2022-05-16 18:46:47 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.a822eaec9e60aa7b7105f958d5b86792.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c0cac0176a35c0766addbd01d023866",
+    "url": "https://loteknowledg.github.io/csb-5morkm/index.html"
+  },
+  {
+    "revision": "d751295c732641e1d703",
+    "url": "https://loteknowledg.github.io/csb-5morkm/static/css/main.f20b78b7.chunk.css"
+  },
+  {
+    "revision": "ed7ffe14ce5209ed2284",
+    "url": "https://loteknowledg.github.io/csb-5morkm/static/js/2.b94e72e0.chunk.js"
+  },
+  {
+    "revision": "d751295c732641e1d703",
+    "url": "https://loteknowledg.github.io/csb-5morkm/static/js/main.cbfd8c51.chunk.js"
+  },
+  {
+    "revision": "a08a01c056c72ea266d7",
+    "url": "https://loteknowledg.github.io/csb-5morkm/static/js/runtime~main.c413de4c.js"
+  }
+]);
